4501ecc01f7154cde80d05d931d074937592e67f
vishnusundar2005 <skvishnu0204@gmail.com>
2026-02-16 19:41:33 +0530

    Configure client for Vercel deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 
 // 🔥 AUTO ATTACH TOKEN
-API.interceptors.request.use((req)=>{
+API.interceptors.request.use((req) => {
 
     const token = localStorage.getItem("token");
 
-    if(token){
+    if (token) {
         req.headers.Authorization = `Bearer ${token}`;
     }
 
